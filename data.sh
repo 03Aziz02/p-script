d694f221e648b301abe5a168c51a04575b61eee8
03Aziz02 <157199305+03Aziz02@users.noreply.github.com>
2024-03-18 16:21:29 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum -y install git
-git clone https://github.com/03Aziz02/flight-perdiction-py.git
-cd flight-perdiction-py
+git clone https://github.com/03Aziz02/fish-py.git
+cd fish-py
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
